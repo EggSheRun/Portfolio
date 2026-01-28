add319885cce4155e65b14d1f189e636091f6743
EggSheRun <rhayabusa960@gmail.com>
2026-01-28 14:50:13 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Ken Biñas Portfolio</title>
+</head>
+<body>
+    <div class="header">
+        <a class="logo" href="#ken  ">Ken Biñas</a>
+
+        <nav class="nav-button">
+            <a href="#about">About</a>
+            <a href="#skills">Skills</a>
+            <a href="#projects">Project</a>
+            <a href="#contact">Contact</a>
+        </nav>
+
+        <div class="resume">
+            <a href="#resume">Resume</a>
+        </div>
+
+    </div>
+    
+</body>
+</html>
